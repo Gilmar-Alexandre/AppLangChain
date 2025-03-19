f42ff78ee53554d1ecee006f80f979ab5c6f8353
Gilmar <gilmar.alexandre.sc@outlook.com>
2025-03-18 23:16:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import tempfile
-
 import streamlit as st
 from langchain.memory import ConversationBufferMemory
-
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.agents import Tool
 
 from loaders import *
 
@@ -21,6 +20,26 @@ CONFIG_MODELOS = {'Groq':
                          'chat': ChatOpenAI}}
 
 MEMORIA = ConversationBufferMemory()
+
+PERFIS_AGENTES = {
+    'Especialista em IA': {
+        'descricao': '''Você é um agente especialista em IA, focado em análise de documentação, 
+        versões e especificações técnicas. Forneça informações técnicas precisas e análise 
+        de compatibilidade de versões.''',
+        'ferramentas': ['busca_documentacao', 'verificacao_versao']
+    },
+    'Consultor Criativo': {
+        'descricao': '''Você é um agente consultor criativo que avalia ideias e fornece 
+        feedback construtivo. Foque em inovação, viabilidade e possíveis melhorias.''',
+        'ferramentas': ['avaliacao_ideias', 'analise_mercado']
+    },
+    'Pesquisador Web': {
+        'descricao': '''Você é um agente pesquisador especializado em encontrar e verificar 
+        informações de fontes da internet. Foque em precisão e cite fontes confiáveis.''',
+        'ferramentas': ['busca_web', 'verificacao_fatos']
+    }
+}
+
 
 def carrega_arquivos(tipo_arquivo, arquivo):
     if tipo_arquivo == 'Site':
@@ -45,34 +64,47 @@ def carrega_arquivos(tipo_arquivo, arquivo):
     return documento
 
 def carrega_modelo(provedor, modelo, api_key, tipo_arquivo, arquivo):
-
     documento = carrega_arquivos(tipo_arquivo, arquivo)
+    
+    agentes_selecionados = st.session_state.get('agentes_selecionados', ['Especialista em IA'])
+    
+    instrucoes_agentes = "\n\n".join([
+        f"Quando atuando como {agente}:\n{PERFIS_AGENTES[agente]['descricao']}"
+        for agente in agentes_selecionados
+    ])
 
-    system_message = '''Você é um assistente amigável chamado GascIA.
-    Você possui acesso às seguintes informações vindas 
-    de um documento {}: 
+    system_message = f'''Você é o GascIA, um assistente multifuncional que pode alternar entre diferentes papéis.
+    Você tem acesso às informações de um documento {tipo_arquivo} e pode realizar buscas na internet:
 
     ####
-    {}
+    {documento}
     ####
 
-    Utilize as informações fornecidas para basear as suas respostas.
+    {instrucoes_agentes}
 
-    Sempre que houver $ na sua saída, substita por S.
+    Para realizar buscas na internet, use a ferramenta de busca disponível.
+    Quando usar informações da internet, cite a fonte.
+
+    Com base na pergunta do usuário, adote o papel mais apropriado dentre os agentes selecionados.
+    Sempre indique qual papel de agente você está usando para responder.
+
+    Sempre que houver $ na sua saída, substitua por S.
 
     Se a informação do documento for algo como "Just a moment...Enable JavaScript and cookies to continue" 
-    sugira ao usuário carregar novamente o Oráculo!'''.format(tipo_arquivo, documento)
-
-    print(system_message)
+    sugira ao usuário carregar novamente o GascAI!'''
 
     template = ChatPromptTemplate.from_messages([
         ('system', system_message),
         ('placeholder', '{chat_history}'),
         ('user', '{input}')
     ])
+    
     chat = CONFIG_MODELOS[provedor]['chat'](model=modelo, api_key=api_key)
+    
+    if 'Pesquisador Web' in agentes_selecionados:
+        chat.tools = [FERRAMENTAS['busca_web']]
+    
     chain = template | chat
-
     st.session_state['chain'] = chain
 
 def pagina_chat():
@@ -104,7 +136,7 @@ def pagina_chat():
         st.session_state['memoria'] = memoria
 
 def sidebar():
-    tabs = st.tabs(['Upload de Arquivos', 'Seleção de Modelos'])
+    tabs = st.tabs(['Upload de Arquivos', 'Seleção de Modelos', 'Seleção de Agentes'])
     with tabs[0]:
         tipo_arquivo = st.selectbox('Selecione o tipo de arquivo', TIPOS_ARQUIVOS_VALIDOS)
         if tipo_arquivo == 'Site':
@@ -124,8 +156,15 @@ def sidebar():
             f'Adicione a api key para o provedor {provedor}',
             value=st.session_state.get(f'api_key_{provedor}'))
         st.session_state[f'api_key_{provedor}'] = api_key
+    with tabs[2]:
+        agentes_selecionados = st.multiselect(
+            'Selecione os agentes para sua consulta',
+            options=list(PERFIS_AGENTES.keys()),
+            default=['Especialista em IA']
+        )
+        st.session_state['agentes_selecionados'] = agentes_selecionados
     
-    if st.button('Inicializar Oráculo', use_container_width=True):
+    if st.button('Inicializar GascAI', use_container_width=True):
         carrega_modelo(provedor, modelo, api_key, tipo_arquivo, arquivo)
     if st.button('Apagar Histórico de Conversa', use_container_width=True):
         st.session_state['memoria'] = MEMORIA
